b6be898ae7f33d6d16428ee6a9bf90e88ec3541b
NoobDad <mardaed10@gmail.com>
2022-12-09 23:50:36 -0600

    Implemented GameManager as example for Singleton implementation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -23,6 +24,18 @@ public class GameManager : Singleton<GameManager>
     // Unity Event
     private void OnApplicationQuit()
     {
-        
+        _sessionEndTime = DateTime.Now;
+        TimeSpan timeDifference = _sessionEndTime.Subtract(_sessionStartTime);
+        Debug.Log("Game session lasted: " + timeDifference);
+    }
+
+    private void OnGUI()
+    {
+        if(GUILayout.Button("Next Scene"))
+        {
+            // On button push, load next scene loaded in scene array.
+            int lastIndex = SceneManager.sceneCountInBuildSettings - 1;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex % lastIndex + 1) ;
+        }
     }
 }
