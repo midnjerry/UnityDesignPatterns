d14b3bdb3fd0232862c61c8fa0ddb1ec2a1ce61e
NoobDad <mardaed10@gmail.com>
2022-12-30 14:54:25 -0600

    Added prettify text for Scene title

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -34,7 +35,7 @@ public class GameManager : Singleton<GameManager>
         GUIStyle titleStyle = new GUIStyle();
         titleStyle.fontSize = 24;
         titleStyle.alignment = TextAnchor.UpperCenter;
-        GUI.Label(new Rect((Screen.width - width) / 2, 0, width, 50), "Current Scene: " + SceneManager.GetActiveScene().name, titleStyle);
+        GUI.Label(new Rect((Screen.width - width) / 2, 0, width, 50), "Current Scene: " + ConvertFromPascalCaseToSpaces(SceneManager.GetActiveScene().name), titleStyle);
         
         GUI.color = Color.white; 
         GUILayout.BeginArea(new Rect(Screen.width - 100, 0, 100, 50));
@@ -45,5 +46,10 @@ public class GameManager : Singleton<GameManager>
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex % lastIndex + 1) ;
         }        
         GUILayout.EndArea();        
+    }
+
+    private string ConvertFromPascalCaseToSpaces(string input)
+    { 
+        return Regex.Replace(input, "(?<!^)([A-Z][a-z]|(?<=[a-z])[A-Z])", " $1").Trim();
     }
 }
